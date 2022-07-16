3d5404a915fd4d738c76186bd65f049ef8b09593
ConnorWhalen <caw742@mun.ca>
2022-07-16 00:07:03 -0230

    add basic game background and special tiles

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -2,24 +2,39 @@ extends Node2D
 
 
 onready var Chip = preload("res://scenes/Chip.tscn")
+onready var Special = preload("res://scenes/Special.tscn")
 
 var chips = []
+var specials = []
 
 
 func _ready():
 	$ChipTimer.start()
+	$SpecialTimer.start()
 
 
 func _process(delta):
-	for i in range(chips.size()-1, -1, -1):
-		var chip = chips[i]
-		if chip.dead:
-			remove_child(chip)
-			chips.remove(i)
+	cull(chips)
+	cull(specials)
 			
 
 
 func _on_ChipTimer_timeout():
 	var chip = Chip.instance()
+	chip.z_index = 1
 	add_child(chip)
 	chips.append(chip)
+
+
+func _on_SpecialTimer_timeout():
+	var special = Special.instance()
+	add_child(special)
+	specials.append(special)
+
+
+func cull(nodes):
+	for i in range(nodes.size()-1, -1, -1):
+		var node = nodes[i]
+		if node.dead:
+			remove_child(node)
+			nodes.remove(i)
