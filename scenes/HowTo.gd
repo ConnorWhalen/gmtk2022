504efae57fa9668caddcd8cec1350b131b536e33
ConnorWhalen <caw742@mun.ca>
2022-07-17 11:02:47 -0230

    add dollar sign when special is collected. Fix routing

--- scenes/HowTo.gd
+++ scenes/HowTo.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 signal mode_menu
 
+
 func _on_Back_pressed():
 	emit_signal("mode_menu")
 #	get_tree().change_scene("res://scenes/Menu.tscn")
--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -1,14 +1,16 @@
-extends Control
+extends Node2D
 
 signal mode_game
+signal mode_options
+signal mode_howto
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _on_StartButton_pressed():
-	get_tree().change_scene("res://scenes/Game.tscn")
-
+	emit_signal("mode_game")
+#	get_tree().change_scene("res://scenes/Game.tscn")
 
 
 func _on_QuitButton_pressed():
@@ -16,9 +18,11 @@ func _on_QuitButton_pressed():
 
 
 func _on_OptionsButton_pressed():
-	get_tree().change_scene("res://scenes/options.tscn")
+	emit_signal("mode_options")
+#	get_tree().change_scene("res://scenes/options.tscn")
 
 
 
 func _on_HowToButton_pressed():
-	get_tree().change_scene("res://scenes/HowTo.tscn")
+	emit_signal("mode_howto")
+#	get_tree().change_scene("res://scenes/HowTo.tscn")
